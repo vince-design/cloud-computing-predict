6f5fa92d9b2a6e8e38be3d91d2fe9fb1f676ef87
vince-design <99393595+vince-design@users.noreply.github.com>
2022-05-17 15:29:13 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "{https://rtrff8ctqb.execute-api.eu-west-1.amazonaws.com/default/Lambdafunction1}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
